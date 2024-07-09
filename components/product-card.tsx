194cc0c5213ccd1c2894056ab93456d14d6ca89c
Peter Abah <peterabah.ob@gmail.com>
2024-07-09 10:53:08 +0100

    feat: style product card when product is in cart

--- components/product-card.tsx
+++ components/product-card.tsx
@@ -1,6 +1,8 @@
 import { formatPrice } from "@/lib";
 import { Product } from "@/lib/store";
 import { useAppContext } from "@/lib/store-context";
+import checkIcon from "@/public/images/check.svg";
+import clsx from "clsx";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
@@ -9,10 +11,15 @@ type Props = {
 };
 export default function ProductCard({ product }: Props) {
   const _addToCart = useAppContext((state) => state.addToCart);
+  const cart = useAppContext((state) => state.cart);
   const router = useRouter();
 
+  const isProductInCart = cart.items[product.id] != undefined;
+
   const addToCart = () => {
-    _addToCart(product.id);
+    if (!isProductInCart) {
+      _addToCart(product.id);
+    }
     router.push("/cart");
   };
 
@@ -27,9 +34,22 @@ export default function ProductCard({ product }: Props) {
       </div>
       <button
         onClick={addToCart}
-        className="bg-purple-3 text-white px-4 py-2.5 rounded-lg w-fit text-sm font-semibold hover:box-shadow-purple hover:bg-purple-3/90 transition-all"
+        className={clsx(
+          "text-white px-4 py-2.5 rounded-lg w-fit text-sm font-semibold transition-all flex gap-1 items-center",
+          {
+            "bg-purple-3 hover:box-shadow-purple hover:bg-purple-3/90": !isProductInCart,
+            "bg-gray-3 box-shadow-purple hover:bg-gray-3/60": isProductInCart,
+          }
+        )}
       >
-        Add to cart
+        {isProductInCart ? (
+          <>
+            <Image src={checkIcon} alt="" width={16} height={16} />
+            <span>Item added</span>
+          </>
+        ) : (
+          <span>Add to cart</span>
+        )}
       </button>
     </li>
   );
