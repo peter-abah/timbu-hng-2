5557d0927b05272b6444584f1c6336cb87764880
Peter Abah <peterabah.ob@gmail.com>
2024-07-09 17:15:44 +0100

    feat: change header title to timbu

--- components/header.tsx
+++ components/header.tsx
@@ -3,9 +3,8 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="flex justify-center py-5 border-b border-gray-1">
-      <Link href="/" className="font-semibold text-xl">
-        <span>Gadget</span>
-        <span className="text-purple">HAVEN</span>
+      <Link href="/" className="font-semibold text-xl text-purple-3">
+        Timbu
       </Link>
     </header>
   );
