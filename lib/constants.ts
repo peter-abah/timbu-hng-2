918b8a73102dba0bf36007f7979bd1b958ccfb20
Peter Abah <peterabah.ob@gmail.com>
2024-07-12 15:15:22 +0100

    feat: add api keys

---
+++ lib/constants.ts
@@ -0,0 +1,4 @@
+export const API_KEY = "a1b63035e0a0410d9314c336664b077320240712125545791593";
+export const APP_ID = "89HDXVTWR9I0EI8";
+export const ORGANIZATION_ID = "8338cb4163264a24b7380767e455cee1";
+export const API_URL = "https://api.timbu.cloud/";
